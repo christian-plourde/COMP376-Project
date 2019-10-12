935526a79279d9aa4552054d1ffac034c9dbd5ca
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-10-12 12:25:00 -0400

    entry sign and basic camera movement

--- COMP376-Project/Assets/Scripts/camera_script.cs
+++ COMP376-Project/Assets/Scripts/camera_script.cs
@@ -6,13 +6,26 @@ public class camera_script : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public float move_speed;
+
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Translate(Vector3.right * move_speed * Time.deltaTime);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Translate(Vector3.left * move_speed * Time.deltaTime);
+        }
+            
     }
 }
