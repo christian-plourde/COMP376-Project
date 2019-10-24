4a352fef5f20723221791dafe5a3071353c2062e
AyushKharade <ayush.kharade999@gmail.com>
2019-10-24 00:03:20 -0400

    fixed ladder colliders for better climbing

---
+++ COMP376-Project/Assets/Scripts/Puzzles Script/Chest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    bool opened = false;
+    public int giveIronCount;
+    public int giveSteelCount;
+    public int givePewterCount;
+    public GameObject giveNote;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !opened)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                //Debug.Log("you has opened the chest");
+                opened = true;
+                //play animation
+                // player potion ++;
+
+                // if note not null, instantiate it
+
+                this.GetComponent<Chest>().enabled = false;
+            }
+        }
+    }
+}
