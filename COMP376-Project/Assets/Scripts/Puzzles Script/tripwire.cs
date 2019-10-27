b200256407d58acc06327e4b9c6fb050e695fb5e
AyushKharade <ayush.kharade999@gmail.com>
2019-10-27 15:30:01 -0400

    Added enemy animations & setup a test scene

---
+++ COMP376-Project/Assets/Scripts/Puzzles Script/tripwire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tripwire : MonoBehaviour
+{
+
+    public GameObject wire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log("tripwire hit");
+        Destroy(wire);
+    }
+}
