80469de2b03297642133b5429074a7d0b773fb99
Igniswolf10 <43484136+Igniswolf10@users.noreply.github.com>
2019-10-27 17:26:05 -0400

    added first cutscene

---
+++ COMP376-Project/Assets/Scripts/Cutscene scripts/scenechanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class scenechanger : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    bool videoStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (videoPlayer.isPrepared)
+        {
+            videoStarted = true;
+        }
+        if (!videoPlayer.isPlaying && videoStarted)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
