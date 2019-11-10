e9c3b71510c847015daa23714cdc1830f5b972b6
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-10 15:41:27 -0500

    cemetery gate in scene and interactable

---
+++ COMP376-Project/Assets/Scripts/CemeteryGate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CemeteryGate : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        //while the player is close to the gate, if he presses e it should open the gates
+        if (col.CompareTag("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Animator>().SetBool("opened", true);
+            }
+        }
+    }
+}
