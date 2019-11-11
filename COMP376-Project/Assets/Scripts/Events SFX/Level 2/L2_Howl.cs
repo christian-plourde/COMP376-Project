37e1e255afde7a243ca43532a0a46c59156268b9
AyushKharade <ayush.kharade999@gmail.com>
2019-11-11 15:41:33 -0500

    added canon & other models

---
+++ COMP376-Project/Assets/Scripts/Events SFX/Level 2/L2_Howl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2_Howl : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioManager.instance.Play("Howl");
+        Destroy(this.gameObject);
+
+    }
+}
