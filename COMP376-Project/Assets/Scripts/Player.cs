63da2bde76c78b7c5c6e3c9bbf68a4602cb728a7
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-12 12:49:02 -0500

    added interactable shading for level1

--- COMP376-Project/Assets/Scripts/Player.cs
+++ COMP376-Project/Assets/Scripts/Player.cs
@@ -49,8 +49,8 @@ public class Player : MonoBehaviour
 
 
     //potions
-    public int steelCount;[HideInInspector] public bool usingSteel;[HideInInspector] public float steelPotionTime=15f;
-    public int ironCount;[HideInInspector] public bool usingIron;[HideInInspector] public float ironPotionTime = 30f;         // low for testing purpose, increase later
+    public int steelCount; public bool usingSteel;[HideInInspector] public float steelPotionTime=15f;
+    public int ironCount; public bool usingIron;[HideInInspector] public float ironPotionTime = 30f;         // low for testing purpose, increase later
     public int pewterCount;[HideInInspector] public bool usingPewter;[HideInInspector] public float pewterPotionTime = 20f;
 
     // count limit:
