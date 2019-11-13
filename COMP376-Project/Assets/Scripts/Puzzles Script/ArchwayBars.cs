fd600b411e5ecbdf6ea7b8902be5ea3e854075ac
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-12 23:32:13 -0500

    added first puzzle

---
+++ COMP376-Project/Assets/Scripts/Puzzles Script/ArchwayBars.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchwayBars : MonoBehaviour
+{
+    Animator animator;
+    public bool start_opened;
+    bool opened;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        opened = false;
+        if (start_opened)
+            toggle_gate();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggle_gate()
+    {
+        animator.SetBool("opened", !opened);
+        opened = !opened;
+    }
+
+}
