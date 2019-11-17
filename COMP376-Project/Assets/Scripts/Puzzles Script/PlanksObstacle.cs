d7784c9d59dfdb79c412dabd7c58f661d0f6f4fe
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-17 00:40:30 -0500

    added breaking plank obstacle

---
+++ COMP376-Project/Assets/Scripts/Puzzles Script/PlanksObstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanksObstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            Debug.Log(col.gameObject.GetComponent<Rigidbody>().velocity.magnitude);
+            for (int i = 1; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+        
+    }
+}
