224a1cbfe4e709d7ba3d52c84f7c38a76a50bd06
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-18 05:07:23 -0500

    second puzzle done

---
+++ COMP376-Project/Assets/Scripts/Puzzles Script/l2_p2_key_door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class l2_p2_key_door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("Key"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Animator>().SetBool("opened", true);
+                Destroy(col.gameObject);
+            }
+        }
+    }
+    
+}
