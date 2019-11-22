494ecc6d6d7db708c0e0214b4861f27271a509fd
dvellucci <vellucci.dan@gmail.com>
2019-11-22 17:40:24 -0500

    boss phase 2

--- COMP376-Project/Assets/Scripts/Enemy/Boss.cs
+++ COMP376-Project/Assets/Scripts/Enemy/Boss.cs
@@ -10,14 +10,20 @@ public class Boss : MonoBehaviour
 
     public ParticleSystem m_implosionTeleport;
     public ParticleSystem m_explosionteleport;
+    float m_fadeTime = 3;
+    float _alpha = 0.0f;
+    float _startAlpha = 0f;
+    float _endAlpha = 1.0f;
+    MeshRenderer m_bossRenderer;
 
     EnemyHealth m_health;
     BossPhase1 m_phase1;
     BossPhase2 m_phase2;
-    BossPhase3 m_phase3;
+    int m_numOfTimesHit;
 
     int m_phase;
-    int m_currentPhase;
+    int m_previousPhase;
+    bool m_isPhase2;
     bool m_isPlayerHit;
     float m_playerHitTimer;
     float m_playerHitCooldown;
@@ -44,28 +50,31 @@ public class Boss : MonoBehaviour
     bool m_implosionCreated = false;
     bool m_explosionCreated = false;
 
+    Vector3 m_bossPosition;
+    Vector3 m_teleportOffset;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_numOfTimesHit = 0;
         IsIdle = true;
         Dashing = false;
         IsSlamAttack = false;
 
         m_phase = 1;
+        m_previousPhase = 0;
         m_health = GetComponent<EnemyHealth>();
         m_phase1 = GetComponent<BossPhase1>();
         m_phase2 = GetComponent<BossPhase2>();
-        m_phase3 = GetComponent<BossPhase3>();
 
         m_phase1.enabled = false;
         m_phase2.enabled = false;
-        m_phase3.enabled = false;
 
         m_isPlayerHit = false;
         m_playerHitCooldown = 2;
         m_playerHitTimer = m_playerHitCooldown;
 
+        m_isPhase2 = false;
         m_isBossHit = false;
         m_bossHitCooldown = 2;
         m_bossHitTimer = m_bossHitCooldown;
@@ -78,12 +87,16 @@ public class Boss : MonoBehaviour
 
         //audio
         AudioManager.instance.Play("Boss_Phase1_Music");
+
+        m_teleportOffset = new Vector3(0, 1, 0);
+
+        m_bossRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        m_bossPosition = transform.position;
         if (m_isPlayerHit)
         {
             m_playerHitTimer -= Time.deltaTime;
@@ -98,22 +111,20 @@ public class Boss : MonoBehaviour
                 m_isBossHit = false;
         }
 
-        if (m_health.GetCurrentHealth() <= 7)
+        if (GetComponent<EnemyHealth>().GetCurrentHealth() <= 7 && !m_isPhase2)
         {
-            m_phase = -100;
+            m_previousPhase = m_phase;
+            m_phase = -1;
+            m_isPhase2 = true;
         }
 
         if (m_phase == 1)
         {
-            m_phase1.enabled = true;
+            m_phase2.enabled = true;
         }
         else if (m_phase == 2)
         {
-            m_phase1.enabled = true;
-        }
-        else if (m_phase == 3)
-        {
-            m_phase1.enabled = true;
+            m_phase2.enabled = true;
         }
         else
         {
@@ -139,7 +150,9 @@ public class Boss : MonoBehaviour
         if (collision.tag == "JoraFist" && !m_isImmune)
         {
             m_isBossHit = true;
+            collision.gameObject.SetActive(false);
             m_health.TakeDamage(1);
+            m_numOfTimesHit++;
             m_bossHitTimer = m_bossHitCooldown;
             Debug.Log("enemy punched");
         }
@@ -156,10 +169,8 @@ public class Boss : MonoBehaviour
 
     void StartNextPhase()
     {
-        if (m_phase == 1)
+        if (m_previousPhase == 1)
             m_phase = 2;
-        else if (m_phase == 2)
-            m_phase = 3;
     }
 
    // events
@@ -189,18 +200,20 @@ public class Boss : MonoBehaviour
 
         m_damagingFloor.gameObject.SetActive(false);
 
-        if(!m_implosionCreated)
+        StartCoroutine(FadeTo(0.0f, 2.0f));
+        if (!m_implosionCreated)
         {
-            Instantiate(m_implosionTeleport, transform.position, transform.rotation);
-            m_implosionTeleport.Emit(1);
+            Instantiate(m_implosionTeleport, m_bossPosition + m_teleportOffset, transform.rotation);
+            m_implosionTeleport.Emit(1);    
             m_implosionCreated = true;
         }
 
 
         if (!m_explosionCreated)
         {
-            Instantiate(m_explosionteleport, m_startPoint.position, transform.rotation);
+            Instantiate(m_explosionteleport, m_startPoint.position + m_teleportOffset, transform.rotation);
             m_explosionteleport.Emit(1);
+            StartCoroutine(FadeTo(1.0f, 2.0f));
             m_explosionCreated = true;
         }
 
@@ -212,7 +225,6 @@ public class Boss : MonoBehaviour
 
         m_phase1.enabled = false;
         m_phase2.enabled = false;
-        m_phase3.enabled = false;
 
         yield return new WaitForSeconds(3.5f);
 
@@ -253,4 +265,16 @@ public class Boss : MonoBehaviour
             this.GetComponent<Animator>().SetBool("usingHammer", m_slamAttack);
         }
     }
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        float alpha = transform.GetComponent<MeshRenderer>().material.color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+            transform.GetComponent<MeshRenderer>().material.color = newColor;
+            yield return null;
+        }
+    }
+
 }
--- COMP376-Project/Assets/Scripts/Enemy/BossPhase2.cs
+++ COMP376-Project/Assets/Scripts/Enemy/BossPhase2.cs
@@ -4,16 +4,146 @@ using UnityEngine;
 
 public class BossPhase2 : MonoBehaviour
 {
+    public Transform m_camera;
     public Transform[] m_teleportPoints;
+    public Transform[] m_damagingFloors;
+    public Transform[] m_breakingPlatforms;
+
+    Boss m_bossScript;
+    ParticleSystem m_implosionTeleport;
+    ParticleSystem m_explosionTeleport;
+
+    int m_teleportCounter;
+    int m_TeleportSpotIndex;
+
+    bool m_isTeleporting;
+    bool m_isVulnerable;
+
+    bool m_implosionCreated = false;
+    bool m_explosionCreated = false;
+
+    bool m_start;
+
+    float m_startTimer;
+
+    float m_teleportCooldown;
+    float m_teleportTimer;
+
+    float m_implodeTimer;
+    float m_implodeCooldown;
+
+    Vector3 m_bossPosition;
+    Vector3 m_teleportOffset;
+
+    ParticleSystem implosion;
+    ParticleSystem explosion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_implodeTimer = 0;
+        m_implodeCooldown = 2.0f;
+        m_teleportCooldown = 3f;
+        m_teleportTimer = 0;
+
+        m_bossScript = this.transform.GetComponent<Boss>();
+        m_implosionTeleport = m_bossScript.m_implosionTeleport;
+        m_explosionTeleport = m_bossScript.m_explosionteleport;
+ 
+        m_teleportCounter = 0;
+        m_TeleportSpotIndex = 0;
+
+        m_isTeleporting = false;
+        m_isVulnerable = false;
+
+        m_start = false;
+
+        m_startTimer = 5;
+
+        this.GetComponent<Boss>().m_isImmune = true;
+
+        Debug.Log("Phase 2 begins.");
+
+        m_teleportOffset = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_bossPosition = transform.position;
+        if (!m_start)
+        {
+            m_startTimer -= Time.deltaTime;
+            if (m_startTimer <= 0)
+            {
+                m_isTeleporting = true;
+                m_start = true;
+
+                ParticleSystem.MainModule implode = m_implosionTeleport.main;
+                implode.startSize = 4f;
+                implode.duration = 0.5f;
+
+                ParticleSystem.MainModule explode = m_explosionTeleport.main;
+                explode.startSize = 4f;
+                explode.duration = 0.2f;
+            }
+        }
+
+        if (m_isTeleporting)
+        {
+            this.GetComponent<Boss>().m_isImmune = true;
+            Teleport();
+        }
+        else
+        {
+            this.GetComponent<Boss>().m_isImmune = false;
+        }
+    }
+
+    void Teleport()
+    {
+        m_teleportTimer += Time.deltaTime;
+
+        if (!m_implosionCreated)
+        {
+            implosion = Instantiate(m_implosionTeleport, m_bossPosition + m_teleportOffset, transform.rotation);
+            m_implosionTeleport.Emit(1);
+            m_implosionCreated = true;
+        }
+        else
+        {
+            m_implodeTimer += Time.deltaTime;
+            if(m_implodeTimer >= m_implodeCooldown)
+            {
+                this.transform.position = new Vector3(50, 50, 50);
+            }
+        }
+
+        if (!m_explosionCreated)
+        {
+            explosion = Instantiate(m_explosionTeleport, m_teleportPoints[m_TeleportSpotIndex].position + m_teleportOffset, transform.rotation);
+            m_explosionTeleport.Emit(1);
+            m_explosionCreated = true;
+        }
+
+        if (m_teleportTimer >= m_teleportCooldown)
+        {
+            //this.transform.rotation = Quaternion.Euler(0, -90, 0);
+            this.transform.position = m_teleportPoints[m_TeleportSpotIndex].position;
+            m_teleportTimer = 0;
+            m_TeleportSpotIndex++;
+            if (m_TeleportSpotIndex > 2)
+                m_TeleportSpotIndex = 0;
+            m_implodeTimer = 0;
+            m_implosionCreated = false;
+            m_explosionCreated = false;
+            StartCoroutine(DestroyParticles(implosion));
+            StartCoroutine(DestroyParticles(explosion));
+        }
+    }
+    IEnumerator DestroyParticles(ParticleSystem particles)
+    {
+        yield return new WaitForSeconds(1.0f);
+        Destroy(particles);
     }
 }
