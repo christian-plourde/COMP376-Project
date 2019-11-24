92963b3930d3a2a507f9121b01526f4cd5860b6b
AyushKharade <ayush.kharade999@gmail.com>
2019-11-23 23:10:32 -0500

    added coinshot

---
+++ COMP376-Project/Assets/Scripts/Enemy/CoinScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+
+    float deletionTime = 1.8f;
+    float counter;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > deletionTime)
+            Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().registerHit();
+            Destroy(this.gameObject);
+        }
+      
+    }
+}
---
+++ COMP376-Project/Assets/Scripts/Enemy/CoinshotEyesight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinshotEyesight : MonoBehaviour
+{
+
+    Coinshot CoinshotScriptRef;
+
+    void Start()
+    {
+        CoinshotScriptRef = gameObject.GetComponentInParent<Coinshot>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            CoinshotScriptRef.Target = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            CoinshotScriptRef.Target = null;
+    }
+}
