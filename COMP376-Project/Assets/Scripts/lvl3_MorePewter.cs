cedbca18ceee92a788169096a73cd986e09ff73a
AyushKharade <ayush.kharade999@gmail.com>
2019-12-01 10:36:19 -0500

    changed starting potions

---
+++ COMP376-Project/Assets/Scripts/lvl3_MorePewter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvl3_MorePewter : MonoBehaviour
+{
+
+    public GameObject PlayerRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerRef.GetComponent<Player>().pewterCount <= 0 && !PlayerRef.GetComponent<Player>().usingPewter)
+        {
+            PlayerRef.GetComponent<Player>().pewterCount += 1;
+        }
+    }
+}
